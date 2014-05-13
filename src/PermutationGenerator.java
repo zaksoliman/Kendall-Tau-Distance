758f1aa03acf9d19337b5a271dcfaeb6ef816bb6
zakaria <zakaria@crunchbang.(none)>
2014-05-13 18:24:16 -0400

    added some new functions to print out new data

--- src/PermutationGenerator.java
+++ src/PermutationGenerator.java
@@ -4,6 +4,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
+import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
+
 
 public class PermutationGenerator {
 	
@@ -12,22 +14,72 @@ public class PermutationGenerator {
 		this.filePath = filePath;
 	}
 	
-	public ArrayList<ArrayList<Integer>>  generatePermutations(int size, int numOfPrmutations)
+	public ArrayList<ArrayList<ArrayList<Integer>>> getAllPermutationSets(int setSize, int permutationSize)
 	{
+		ArrayList<ArrayList<Integer>> allPermutations = getAllPermutations(permutationSize);
+		ArrayList<Integer> tempPermutation1, tempPermutation2, tempPermutation3;
+		ArrayList<ArrayList<Integer>> tempPermSet = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<ArrayList<Integer>>> sets = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		
+		for(int i=0; i<allPermutations.size()-2 ; i++){
+			
+			tempPermutation1 = allPermutations.get(i);
+			tempPermSet.add(new ArrayList<Integer>(tempPermutation1));
+			
+			for (int j=i+1; j <allPermutations.size()-1; j++){
+				
+				tempPermutation2 = allPermutations.get(j);
+				tempPermSet.add(new ArrayList<Integer>(tempPermutation2));
+				
+				for (int k = j+1; k < allPermutations.size(); k++){
+					
+					tempPermutation3 = allPermutations.get(k);
+					tempPermSet.add(new ArrayList<Integer>(tempPermutation3));
+					sets.add(new ArrayList<ArrayList<Integer>>(tempPermSet));
+					tempPermSet.remove(tempPermSet.size()-1);										
+				}
+				tempPermSet.remove(tempPermSet.size()-1);	
+			}
+			tempPermSet.clear();	
+		}
+		
+		return sets;
+	}
+	
+	public ArrayList<ArrayList<Integer>>  generateRandomPermutationSet(int size, int numOfPrmutations)
+	{
+		generatedPermutations.clear();
 		permutation = generateInitialPermutation(size);
 		
 		permute(0);
 		
 		Random rnd = new Random();
 		int toRemove = generatedPermutations.size() - numOfPrmutations;
+		int elemIndex; //holds index of element to be randomly removed
 		
 		for (int i = 0; i < toRemove; i++) {			
-			generatedPermutations.remove(rnd.nextInt(generatedPermutations.size()));			
+			elemIndex = rnd.nextInt(generatedPermutations.size());
+			complementSet.add(generatedPermutations.get(elemIndex));	
+			generatedPermutations.remove(elemIndex);					
 		}
 		
 		//permutationSet = new HashSet<ArrayList<Integer>>(generatedPermutations);
 		
 		return generatedPermutations;		
+	}
+
+	public ArrayList<ArrayList<Integer>> getComplement()
+	{
+		return complementSet;
+	}
+	
+	private ArrayList<ArrayList<Integer>> getAllPermutations(int size)
+	{
+		generatedPermutations.clear();
+		permutation = generateInitialPermutation(size);
+		permute(0);
+		
+		return generatedPermutations;
 	}
 	
 	private ArrayList<Integer> generateInitialPermutation(int size)
@@ -85,5 +137,6 @@ public class PermutationGenerator {
 	private ArrayList<Integer> permutation;
 	private String filePath;
 	private ArrayList<ArrayList<Integer>> generatedPermutations = new ArrayList<ArrayList<Integer>>();
+	private ArrayList<ArrayList<Integer>> complementSet = new ArrayList<ArrayList<Integer>>();
 	//private HashSet<ArrayList<Integer>> permutationSet;
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,151 +1,144 @@
 import java.io.*;
 import java.util.*;
+
+import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
 public class Tests 
 {
 	public static void main(String[] args)
 	{	
+		//test(read("Resultats/Redo Number of medians 9.txt"));	
+		
+		printResultsSubsets();
+	}
+	
+	private static void printResultsRandomPermutations()
+	{
 		String filePath = "Results.txt";
 		File file = new File(filePath);
+		PermutationGenerator pg = new PermutationGenerator("");
+		MedianFinderBT medFinder;
 		
 		ArrayList<ArrayList<Integer>> permSetA = new ArrayList<ArrayList<Integer>>();
 		ArrayList<ArrayList<Integer>> permSetB = new ArrayList<ArrayList<Integer>>();
 		
-		//ArrayList<ArrayList<Integer>> medOfSetA = new ArrayList<ArrayList<Integer>>();
-		//ArrayList<ArrayList<Integer>> medOfSetB = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> medSetA, medSetB, results, exp;
 		
 		Random rnd = new Random();
-		int sizeOfSet; // = rnd.nextInt(12)+1;
+		int sizeOfSet = rnd.nextInt(12)+1;
 		int sizeOfPermutation = 4;
 		
 		PermutationGenerator pgA = new PermutationGenerator("permutations.txt");
 		PermutationGenerator pgB = new PermutationGenerator("permutations.txt");
-		//permSetA = pgA.generatePermutations(sizeOfPermutation, sizeOfSet);
-		//permSetB = pgB.generatePermutations(sizeOfPermutation, sizeOfSet);
+		permSetA = pgA.generateRandomPermutationSet(sizeOfPermutation, sizeOfSet);
+		permSetB = pgB.generateRandomPermutationSet(sizeOfPermutation, sizeOfSet);
 		
 		//ArrayList<ArrayList<Integer>> AinterB = inter(permSetA, permSetB);
-		ArrayList<ArrayList<Integer>> AunionB; //= union(permSetA, permSetB);
-		
-		/*System.out.print(permSetA + "\n");
-		System.out.println(permSetB);
-		
-		System.out.println(AinterB);
-		System.out.println(AunionB);*/
-			
+		ArrayList<ArrayList<Integer>> AunionB;
+		//ArrayList<ArrayList<Integer>> notA;
+		//ArrayList<ArrayList<Integer>> notB;
 		
 		try
 		{
 			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			int numOfIterations = 5;
+			bw.write("Median Set, Distance, Median set size, Permutation Size \n");
 			
 			for(int i=0; i< numOfIterations ; i++)
 			{
 				
-				sizeOfSet = rnd.nextInt(12)+1;
-				
-				permSetA = pgA.generatePermutations(sizeOfPermutation, sizeOfSet);
-				permSetB = pgB.generatePermutations(sizeOfPermutation, sizeOfSet);
+				sizeOfSet = rnd.nextInt(12)+1;				
+				permSetA = pgA.generateRandomPermutationSet(sizeOfPermutation, sizeOfSet);
+				permSetB = pgB.generateRandomPermutationSet(sizeOfPermutation, sizeOfSet);
 				AunionB = union(permSetA, permSetB);
+				
+				
+				
 				bw.write("**m=" + permSetA.size() + " n=" + permSetA.get(0).size() + "\n\n");
 				
 				bw.write("A=" + permSetA + "\n");
-				bw.write("B=" + permSetB + "\n\n");
-			
+				bw.write("B=" + permSetB + "\n");			
 				bw.write("AUB=" + AunionB + "\n\n");
-				//bw.write("A inter B=" + AinterB + "\n\n");
-			
-				MedianFinderBT medFinder = new MedianFinderBT(permSetA);			
-				ArrayList<ArrayList<Integer>> results = medFinder.FindMed();			
-				bw.write("M(A)=" + results + ",Distance:" + medFinder.getKendallTauDistance() + "\n\n");
+				
+				bw.write("Median Set, Distance, Median set size, Permutation Size \n");
+				
+				medFinder = new MedianFinderBT(permSetA);			
+				medSetA = medFinder.FindMed();			
+				bw.write("M(A)=" + medSetA + ",D_kt=" + medFinder.getKendallTauDistance() + ",#=" + medSetA.size() + "\n");
 			
 				medFinder = new MedianFinderBT(permSetB);
-				results = medFinder.FindMed();
-				bw.write("M(B)=" + results + ",Distance:" + medFinder.getKendallTauDistance() + "\n\n");
+				medSetB = medFinder.FindMed();
+				bw.write("M(B)=" + medSetB + ",D_kt=" + medFinder.getKendallTauDistance() + ",#=" + medSetB.size() + "\n");
 			
 				medFinder = new MedianFinderBT(AunionB);
 				results = medFinder.FindMed();
-				bw.write("M(A U B)=" + results + ",Distance:" + medFinder.getKendallTauDistance() + "\n\n");
+				bw.write("M(AUB)=" + results + ",D_kt=" + medFinder.getKendallTauDistance() + ",#=" + results.size() + "\n");
+				
+				exp = union(medSetA, medSetB);
+				medFinder = new MedianFinderBT(exp);
+				results = medFinder.FindMed();				
+				bw.write("M(M(A) U M(B))=" + results + ",D_kt=" + medFinder.getKendallTauDistance() + ",#=" + results.size() + "\n\n");
 			}
+			
 			bw.close();
 		}
 		catch (IOException e)
 		{
 			System.out.println(e.getMessage());
-		}	
-			
-		
-		
-		/*
-		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{2,4,5,3,6,1})));
-		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{4,2,1,3,6,5}))); 
-		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{6,1,5,4,2,3})));
-		*/
-		
-		/*permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 2, 3, 4})));
-		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 2, 4, 3}))); 
-		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 4, 2, 3})));*/
-		
-		/*MedianFinderBT medFinder = new MedianFinderBT(permSet.get(0).size(), permSet);
-		medFinder.FindMed();
-		
-		System.out.println("Kendall tau distance = " + medFinder.getKendallTauDistance());
-		
-		ArrayList<ArrayList<Integer>> solutions = medFinder.solutions;
-		
-		for(ArrayList<Integer> sol : solutions)
-		{
-			System.out.println(Arrays.deepToString(sol.toArray()));
 		}
 		
-		System.out.println("done with backtrack!");
+	}
+
+	private static void printResultsSubsets()
+	{
+		String filePath = "Permutation 3.txt";
+		File file = new File(filePath);
+		PermutationGenerator pg = new PermutationGenerator("");
+		MedianFinderBT medFinder;
 		
-		MedianFinderHeuristic medianFinderH = new MedianFinderHeuristic(3, permSet.get(0).size(), permSet);
+		ArrayList<ArrayList<ArrayList<Integer>>> testCases;
 		
-		solutions = medianFinderH.FindMedian();
-		
-		System.out.println("Kendall tau distance = " + medianFinderH.getDistance());
-		
-		for(ArrayList<Integer> sol : solutions)
+		ArrayList<ArrayList<Integer>> results;
+				
+		try
 		{
-			System.out.println(Arrays.deepToString(sol.toArray()));
-		}*/
-		
-		/*MedianFinderBT medFinder;
-		
-		ArrayList<ArrayList<ArrayList<Integer>>> testCases = read("Results/Triplets11.txt");
-		
-		for (ArrayList<ArrayList<Integer>> permSet : testCases) {
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			testCases = pg.getAllPermutationSets(3, 6);
+			bw.write("Median Set, Distance, Median set size, Permutation Size \n");
 			
-			medFinder = new MedianFinderBT(permSet.get(0).size(), permSet);
-		
-			medFinder.FindMed();
-		
-			System.out.println("For the set: " + permSet);
-			
-			System.out.println("Kendall tau distance = " + medFinder.getKendallTauDistance());
-		
-			ArrayList<ArrayList<Integer>> solutions = medFinder.solutions;
-		
-			for(ArrayList<Integer> sol : solutions)
-			{
-				System.out.println(Arrays.deepToString(sol.toArray()));
+			for (ArrayList<ArrayList<Integer>> permSet : testCases) {
+				medFinder = new MedianFinderBT(permSet);
+				
+				results = medFinder.FindMed();		
+				
+				bw.write(results + ",D_kt=" + medFinder.getKendallTauDistance() + ",#=" + results.size() + "\n");
 			}
-		
-			System.out.println("done with backtrack!\n");
-		}*/
+			
+			
+			bw.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 	
-	private static ArrayList<ArrayList<ArrayList<Integer>>> read(String filePath)
+	
+ 	private static ArrayList<ArrayList<ArrayList<Integer>>> read(String filePath)
 	{
-		//String filePath = "Results/Redo Number of medians 4.txt";
 		String input;
+		//Hold all the permutation sets
 		ArrayList<ArrayList<ArrayList<Integer>>> testCases = new ArrayList<ArrayList<ArrayList<Integer>>>();
 		ArrayList<ArrayList<Integer>> permutationSet = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> medSet = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> permutation = new ArrayList<Integer>();
 		
 		File file = new File(filePath);
-		String[] lineSplit = null;
+		String[] inputData = null;
 		String[] permSetString = null;
+		String[] medSetString = null;
+		//String[] medStringArray = null;
 		
 		try
 		{
@@ -155,21 +148,29 @@ public class Tests
 			br.readLine();
 			String tst = br.readLine();
 			
-			System.out.print(tst);
+			//System.out.print(tst);
+			HashMap<String, Object> result = new HashMap<String, Object>();
 			
 			while(!(input = br.readLine()).isEmpty())
 			{
-				lineSplit = input.split("\\t");		
+				inputData = input.split("\\t");		
 
-				permSetString = lineSplit[0].split("],\\s");
-			
+				permSetString = inputData[0].split("],\\s");
+				int startOfMedSet = inputData[1].indexOf('[');
+				int endOfMedSet = inputData[1].lastIndexOf(']');
+				medSetString = inputData[1].substring(startOfMedSet, endOfMedSet-1).split("],\\s"); //We get the median set
+				
+				String medDistString = inputData[1].split("distance")[1].split(" ")[1]; //we get the distance
+				
 				for(String s : permSetString)
 				{
 					s = s.replace("[", "");
-					s = s.replace("]", "");
-					String[] parsed = s.split(",\\s");
+					s = s.replace("]", "");					
+					
+					String[] parsedPermSet = s.split(",\\s");
+					
 				
-					for (String elem : parsed)
+					for (String elem : parsedPermSet)
 					{
 						permutation.add(Integer.parseInt(elem));
 					}
@@ -178,7 +179,28 @@ public class Tests
 					permutation.clear();
 				}
 				
+				for(String s : medSetString)
+				{
+					s = s.replace("[", "");
+					s = s.replace("]", "");				
+					
+					String[] parsedMedSet = s.split(",\\s");
+				
+					for (String elem : parsedMedSet)
+					{
+						permutation.add(Integer.parseInt(elem));
+					}
+				
+					medSet.add(new ArrayList<Integer>(permutation));
+					permutation.clear();
+				}
+				
 				testCases.add(new ArrayList<ArrayList<Integer>>(permutationSet));
+				result.put(MEDIAN_SET_KEY, new ArrayList<ArrayList<Integer>>(medSet));
+				result.put(DISTANCE_KEY, (Integer) Integer.parseInt(medDistString));
+				testResults.add(new HashMap<String, Object>(result));
+				medSet.clear();
+				result.clear();
 				permutationSet.clear();
 			}
 			
@@ -221,7 +243,7 @@ public class Tests
 		return intersection;	
 	}
 
-	/*void read()
+	void readPermutations()
 	{
 		String filePath = "permutation.txt";
 		File file = new File(filePath);
@@ -229,11 +251,16 @@ public class Tests
 		if(file.exists())
 		{
 			file.delete();
-			file.createNewFile();
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		PermutationGenerator p = new PermutationGenerator(filePath);
-		p.generatePermutations(4, 1);
+		p.generateRandomPermutationSet(4, 1);
 		
 		String list;
 		ArrayList<ArrayList<Integer>> permutationSet = new ArrayList<ArrayList<Integer>>(); 
@@ -267,5 +294,103 @@ public class Tests
 		{
 			System.out.println(e.getMessage());
 		}
-	}*/
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void test(ArrayList<ArrayList<ArrayList<Integer>>> testCases)
+	{
+		
+		//ArrayList<ArrayList<Integer>> permSet = new ArrayList<ArrayList<Integer>>();
+		
+		MedianFinderBT medFinder;
+		ArrayList<ArrayList<Integer>> solutions, correctSolutions;
+		int distance, correctDistance;
+		int index = 0;
+		boolean correct = false;
+		boolean allCorrect = true;
+		
+		for (ArrayList<ArrayList<Integer>> permutationSet : testCases) {
+		
+			medFinder = new MedianFinderBT(permutationSet);	
+			solutions = medFinder.FindMed();
+			distance = medFinder.getKendallTauDistance();
+			
+			correctSolutions = (ArrayList<ArrayList<Integer>>) testResults.get(index).get(MEDIAN_SET_KEY);
+			correctDistance = (Integer) testResults.get(index).get(DISTANCE_KEY);
+			
+			if(correctDistance == distance)
+				correct = true;
+			else
+				correct = false;
+			
+			for (ArrayList<Integer> sol : correctSolutions) {
+				if(solutions.contains(sol))
+					correct = true;
+				else
+					correct = false;
+			}
+			
+			if(correct)
+				System.out.println("OK!");
+			else if(!correct){
+				System.out.println("INCORRECT");
+				allCorrect = false;
+			}
+	
+			//System.out.println("For the set: " + permutationSet);		
+			//System.out.println("Kendall tau distance = " + distance);
+	
+			/*for(ArrayList<Integer> sol : solutions)
+			{
+				System.out.println(Arrays.deepToString(sol.toArray()));
+			}*/
+	
+			System.out.println("done with backtrack!\n");			
+			index++;
+		}
+		
+		if(allCorrect)
+			System.out.println("All cases are correct");
+		else if(!allCorrect)
+			System.out.println("One or more incorrect cases");
+		/*permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{2,4,5,3,6,1})));
+		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{4,2,1,3,6,5}))); 
+		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{6,1,5,4,2,3})));
+	
+	
+		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 2, 3, 4})));
+		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 2, 4, 3}))); 
+		permSet.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1, 4, 2, 3})));
+	
+		MedianFinderBT medFinder = new MedianFinderBT(permSet);
+		medFinder.FindMed();
+	
+		System.out.println("Kendall tau distance = " + medFinder.getKendallTauDistance());
+	
+		ArrayList<ArrayList<Integer>> solutions = medFinder.solutions;
+	
+		for(ArrayList<Integer> sol : solutions)
+		{
+			System.out.println(Arrays.deepToString(sol.toArray()));
+		}
+	
+		System.out.println("done with backtrack!");
+	
+		MedianFinderHeuristic medianFinderH = new MedianFinderHeuristic(3, permSet.get(0).size(), permSet);
+	
+		solutions = medianFinderH.FindMedian();
+	
+		System.out.println("Kendall tau distance = " + medianFinderH.getDistance());
+	
+		for(ArrayList<Integer> sol : solutions)
+		{
+			System.out.println(Arrays.deepToString(sol.toArray()));
+		}*/
+		
+		
+	}
+	
+	static final private String MEDIAN_SET_KEY = "MedianSet"; 
+	static final private String DISTANCE_KEY = "distance";
+	static private ArrayList<HashMap<String, Object>> testResults = new ArrayList<HashMap<String,Object>>();
 }
