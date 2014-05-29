b020faeadd53b4b4040c6fa7a5ede83c93943b4a
Zakaria Soliman <zakaria.soliman1@gmail.com>
2014-05-29 13:42:50 -0400

    new script to convert generated permutations to correct format

--- scripts/convertPermutationFormat.py
+++ scripts/convertPermutationFormat.py
@@ -1,12 +1,44 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
 
-# TODO: visit every directory of the parent directory recursivly and process
-# all the files that are found
+from  os import listdir
+from glob import glob
+from re import sub
 
-directory = "../Res60/Result5_60.txt"
+###########
+# Funtion #
+###########
 
-#open the file
-a_file = open("Result5_60.txt")
+def convertFile(filePath):
+    f = open(filePath)
+    lines = f.readlines()
+    f.close()
+    f = open(filePath, 'w')
 
-#TODO: rewrite in a new file the lines with after reformating the set of array
-#in a format that can be easily manipulated by python
+
+    for index, line in enumerate(lines):
+        if line.startswith('{'):
+            line = sub('(\d*)\[(\d*)\](\d*)', r'\2',line)
+            line = sub('{','[', line)
+            line = sub('}',']',line)
+            #replace old line with new line
+            lines[index] = line
+    #Now we write to the file
+    for line in lines:
+        f.write(line)
+    f.close()
+
+########
+# MAIN #
+########
+
+#Get all the subdirecotries to start converting
+directory = "../Resultats/Res/"
+dirList = listdir(directory)
+
+for folder in dirList:
+    filePaths = directory+folder
+    #Get all the txt files
+    filePaths = glob(filePaths+"/*.txt")
+    for a_file in filePaths:
+        print("Now visiting file: " + a_file)
+        convertFile(a_file)
