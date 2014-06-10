18bb0ef12d10606f072313ec727d6b264d158610
Zakaria Soliman <zakaria.soliman1@gmail.com>
2014-06-09 20:23:13 -0400

    sync

--- scripts/KendallTauDistance.py
+++ scripts/KendallTauDistance.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-permutationSet=[[1,6,7,3,13,9,10,8,5,12,4,11,2],[2,11,1,7,10,5,3,6,12,13,4,8,9],[12,2,10,4,8,11,3,7,5,1,6,13,9]]
-medianSet =[[10, 11, 1, 7, 2, 5, 3, 6, 12, 13, 4, 8, 9], [3, 11, 1, 7, 10, 5, 2, 6, 12, 13, 4, 8, 9], [12, 11, 1, 7, 10, 5, 3, 6, 2, 13, 4, 8, 9], [8, 11, 1, 7, 10, 5, 3, 6, 12, 13, 4, 2, 9], [2, 10, 1, 7, 11, 5, 3, 6, 12, 13, 4, 8, 9], [2, 3, 1, 7, 10, 5, 11, 6, 12, 13, 4, 8, 9], [2, 12, 1, 7, 10, 5, 3, 6, 11, 13, 4, 8, 9], [2, 4, 1, 7, 10, 5, 3, 6, 12, 13, 11, 8, 9], [2, 8, 1, 7, 10, 5, 3, 6, 12, 13, 4, 11, 9], [2, 11, 3, 7, 10, 5, 1, 6, 12, 13, 4, 8, 9], [2, 11, 1, 7, 10, 3, 5, 6, 12, 13, 4, 8, 9], [2, 11, 1, 7, 10, 6, 3, 5, 12, 13, 4, 8, 9], [2, 11, 1, 7, 10, 12, 3, 6, 5, 13, 4, 8, 9], [2, 11, 1, 7, 10, 8, 3, 6, 12, 13, 4, 5, 9]]
+from sys import argv
+from ast import literal_eval 
 
 def inversePermutation(permutation):
-    inverse = list(range(len(permutation)))
+
+    inverse = [x for x in range(len(permutation))]
 
     for i in range(0,len(permutation)):
         inverse[((permutation[i])-1)] = i
@@ -11,23 +12,40 @@ def inversePermutation(permutation):
     return inverse
 
 def KendallTauDistance(permutation, permSet):
+
     distance = 0
     totalDist = 0
     permA = inversePermutation(permutation)
+
     for p in permSet:
+
         permB = inversePermutation(p)
+
         #compute the distance
         for i in range(0,len(p)-1):
             for j in range(i+1, len(permB)):
-                if(((permA[i]<permA[j])and(permB[i]>permB[j])) or
-                ((permA[i]>permA[j]) and (permB[i]<permB[j]))):
+
+                if(permA[i] < permA[j] and permB[i] > permB[j]) or \
+                  (permA[i] > permA[j] and permB[i] < permB[j]):
+
                     distance+=1
+
         print("Distance between " + str(permutation) + " and "
                 + str(p) + " is " + str(distance))
+
         totalDist += distance
         distance = 0
+
     return totalDist
 
-for perm in medianSet:
-    totalDist = KendallTauDistance(perm, permutationSet)
-    print("D_KT = " + str(totalDist)+ "\n")
+if __name__ == '__main__':
+
+    permutationSet = argv[1]
+    medianSet = argv[2]
+
+    permutationSet = literal_eval(permutationSet)
+    medianSet = literal_eval(medianSet)
+
+    for perm in medianSet:
+       totalDist = KendallTauDistance(perm, permutationSet)
+       print("D_KT = " + str(totalDist)+ "\n")
--- scripts/medianFinder.py
+++ scripts/medianFinder.py
@@ -9,125 +9,147 @@ permSet = []
 dist_KT = float('inf')
 #Holds the size of the permutations
 permSize = 0
+#Holds element we can add to the rigth or to the left of the integer i
+elem_right
+elem_left
 
+class MedianFinder:
 
-def inversePermutation(permutation):
-    inverse = list(range(len(permutation)))
+    def inversePermutation(permutation):
+        inverse = list(range(len(permutation)))
 
-    for i in range(0,len(permutation)):
-        inverse[((permutation[i])-1)] = i
+        for i in range(0,len(permutation)):
+            inverse[((permutation[i])-1)] = i
 
-    return inverse
+        return inverse
 
-def KendallTauDistance(permutation, permSet):
-    distance = 0
-    totalDist = 0
-    permA =inversePermutation(permutation)
-    for p in permSet:
-        permB = inversePermutation(p)
-
-        # COMPUTE DISTANCE
-        for i in range(0,len(p)-1):
-            for j in range(i+1, len(permB)):
-                if(((permA[i]<permA[j])and(permB[i]>permB[j])) or
-                ((permA[i]>permA[j]) and (permB[i]<permB[j]))):
-                    distance+=1
-
-        totalDist += distance
+    def KendallTauDistance(permutation, permSet):
         distance = 0
-    return totalDist
+        totalDist = 0
+        permA =inversePermutation(permutation)
+        for p in permSet:
+            permB = inversePermutation(p)
 
-#builds initial constraints from the given permutation set
-def buildConstraints(permSet):
+            # COMPUTE DISTANCE
+            for i in range(0,len(p)-1):
+                for j in range(i+1, len(permB)):
+                    if(((permA[i]<permA[j])and(permB[i]>permB[j])) or
+                    ((permA[i]>permA[j]) and (permB[i]<permB[j]))):
+                        distance+=1
 
-    #counts the number of times the int i is before j in all permutations in permSet
-    counterMatrix = [[0 for x in range(len(permSize)+1)] for x in range(len(permSize)+1)]
-    #Holds element we can add to the rigth or to the left of the integer i
-    elem_right = [set() for x in range(len(permSize)+1)]
-    elem_left = [set() for x in range(len(permSize)+1)]
-    #TODO: init elem_{right, left}[0] to all
+            totalDist += distance
+            distance = 0
+        return totalDist
 
-    #Start by counting
-    for perm in permSet:
-        for i in range(len(permSize)-1):
-            for j in range(i+1, len(permSize)):
-                counterMatrix[perm[i]][perm[j]]+=1
+    #builds initial constraints from the given permutation set
+    def buildConstraints(permSet):
 
-    majority = math.ceil(len(permSize)/2)
+        #counts the number of times the int i is before j in all permutations in permSet
+        counterMatrix = [[0 for x in range(len(permSize)+1)] for x in range(len(permSize)+1)]
 
-    for i in range(len(counterMatrix)):
-        for j in range(len(counterMatrix)):
+        elem_right = [set() for x in range(len(permSize)+1)]
+        elem_left = [set() for x in range(len(permSize)+1)]
+        #TODO: init elem_{right, left}[0] to all
 
-            if counterMatrix[i][j] >= majority:
-                #Then we have j to the right of i in a majority of cases
-                elem_right[i].add[j]
-                #and i to the left of j
-                elem_left[j].add[i]
-    return elem_left, elem_right
+        #Start by counting
+        for perm in permSet:
+            for i in range(len(permSize)-1):
+                for j in range(i+1, len(permSize)):
+                    counterMatrix[perm[i]][perm[j]]+=1
 
-#Builds and returns the initial instances from which we start building our
-#solution
-def buildInitialInstances(left):
+        majority = math.ceil(len(permSize)/2)
 
-    size = len(left)
-    partialSol = []
-    initialInstances=[]
+        for i in range(len(counterMatrix)):
+            for j in range(len(counterMatrix)):
 
-    #Find (if it exists) the element that will be the first element of a median
-    for i in range(1,size):
+                if counterMatrix[i][j] >= majority:
+                    #Then we have j to the right of i in a majority of cases
+                    elem_right[i].add[j]
+                    #and i to the left of j
+                    elem_left[j].add[i]
+        return elem_left, elem_right
 
-        if len(left[i]) == 0:
-            #We found the first element of a median
-            partialSol.append(i)
+    #Builds and returns the initial instances from which we start building our
+    #solution
+    def buildInitialInstances(left):
 
-            #We start constructing this initial instance
+        size = len(left)
+        partialSol = []
+        initialInstances=[]
 
-            #Continue adding elements to the partial solution until its no longer possible
-            done = False
-            while(!done):
-                lastElementAdded = partialSol[len(partialSol)-1]
-                done = True
+        #Find (if it exists) the element that will be the first element of a median
+        for i in range(1,size):
 
-                for s in left:
-                    if (len(s) == 1) and (lastElementAdded in s):
-                        partialSol.append(left.index(s))
-                        done=False
-                        break
+            if len(left[i]) == 0:
+                #We found the first element of a median
+                partialSol.append(i)
 
-            initialInstances.append(partialSol[:])
-            del partialSol[:]
+                #We start constructing this initial instance
 
-    return initialInstances
+                #Continue adding elements to the partial solution until its no longer possible
+                done = False
+                while(!done):
+                    lastElementAdded = partialSol[len(partialSol)-1]
+                    done = True
 
-#Finds the median by starting from the given initial solution set
-def findMedBT(potentialSolution):
+                    for s in left:
+                        if (len(s) == 1) and (lastElementAdded in s):
+                            partialSol.append(left.index(s))
+                            done=False
+                            break
 
-    #CASE 1: We have a potential solution if it is the correct size
-    if len(potentialSolution) == permSize
-        currentDist = KendallTauDistance(potentialSolution)
+                initialInstances.append(partialSol[:])
+                del partialSol[:]
 
-        #Check if it's a better solution
-        if currentDist < dist_KT:
-            dist_KT = currentDist
-            solutions[:] = []
-            solutions.append(potentialSolution)
+        return initialInstances
+
+    #Finds the median by starting from the given initial solution set
+    def findMedBT(potentialSolution):
+
+        #CASE 1: We have a potential solution if it is the correct size
+        if len(potentialSolution) == permSize
+            currentDist = KendallTauDistance(potentialSolution)
+
+            #Check if it's a better solution
+            if currentDist < dist_KT:
+                dist_KT = currentDist
+                solutions[:] = []
+                solutions.append(potentialSolution)
+                return
+            #Check if it is as good as the solutions we found
+            elif currentDist == dist_KT:
+                solutions.append(potentialSolution)
+                return
+            #If we reach here then the potential solution is not an optimal one
+            else:
+                return
+
+        lastElement = 0
+        #CASE 2: Check if the current partial solution is valid
+        elif not isValid(potentialSolution):
             return
-        #Check if it is as good as the solutions we found
-        elif currentDist == dist_KT:
-            solutions.append(potentialSolution)
-            return
-        #If we reach here then the potential solution is not an optimal one
+        #Find the elements that we can add to the current solution and recurse
+        if len(potentialSolution) != 0:
+            lastElement = potentialSolution[len(potentialSolution)-1]
+
         else:
-            return
+            lastElement = 0
 
-    #CASE 2: Check if the current partial solution is valid
-    #TODO
+        for elem in
 
-def findMedian():
 
-    """Loops through all the inital solutions and calls findMedBT"""
+    def findMedian():
 
-    startingInstances = getInitialInstances()
-    for potentialSolution in startingInstances:
-        findMedBT(potentialSolution)
+        """Loops through all the inital solutions and calls findMedBT"""
 
+        startingInstances = getInitialInstances()
+        for potentialSolution in startingInstances:
+            findMedBT(potentialSolution)
+
+                ########
+                # MAIN #
+                ########
+
+if __name__ == '__main__'
+
+    #TODO: testing
