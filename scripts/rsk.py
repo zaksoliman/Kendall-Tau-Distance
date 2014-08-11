97c0bdde75caacf0ddb1911c4e9391bf30409593
Zakaria Soliman <zakaria.soliman1@gmail.com>
2014-08-11 13:05:59 -0400

    Done with rsk.py

--- scripts/rsk.py
+++ scripts/rsk.py
@@ -2,8 +2,9 @@
 
 from ast import literal_eval
 from bisect import bisect
-import sysi
-from itertools import permutations, combinations
+import sys
+from itertools import permutations, combinations, chain
+from medianFinder import MedianFinder
 
 def RSK(p):
     '''Given a permutation p, spit out a pair of Young tableaux'''
@@ -22,8 +23,8 @@ def RSK(p):
     for i in range(len(p)):
         insert(int(p[i]), i+1)
 
-    P = tuple(tuple(elem) for elem in P)
-    Q = tuple(tuple(elem) for elem in Q)
+    #P = tuple(tuple(elem) for elem in P)
+    #Q = tuple(tuple(elem) for elem in Q)
     return (P,Q)
 
 def getEquivalenceClasses(permSet):
@@ -33,20 +34,44 @@ def getEquivalenceClasses(permSet):
     for p in permSet:
 
         tableau = RSK(p)
+        tableau = str(tableau[0])
 
-        if tableau[0] not in classes:
-            classes[tableau[0]] = list()
-            classes[tableau[0]].append(p)
+        if tableau not in classes:
+            classes[tableau] = list()
+            classes[tableau].append(p)
         else:
-            classes[tableau[0]].append(p)
+            classes[tableau].append(p)
 
     return classes
 
 #TODO: find a better name
 def getAllMedianSet(eqClasses):
 
-    #chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    for key, permSet in eqClasses.items():
 
+        #print('Class '+key + ':\n')
+        powerSet = chain.from_iterable(combinations(permSet, r) for r in range(len(permSet)+1))
+
+        for perms in powerSet:
+            if len(perms) >= 3:
+                mf = MedianFinder(perms)
+                mf.findMedian()
+                #print('Set: ' + str(perms))
+                #print('Medians: ' + str(mf.solutions))
+                #print('RSK Tableau for each median:')
+
+                for med in mf.solutions:
+                    tableau = RSK(med)
+                    #print(str(med)+'\t'+str(tableau)+'\n')
+                    if key == str(tableau[0]):
+                        print('SAME CLASS')
+                    else:
+                        print('***DIFFERENT CLASS***')
+                        print('Class: ' + key)
+                        print('Set: ' + str(perms))
+                        print('Medians: ' + str(mf.solutions))
+                        print(str(tableau))
+    return
 
 if __name__ == '__main__':
 
